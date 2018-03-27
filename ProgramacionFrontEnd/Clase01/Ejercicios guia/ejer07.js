084f11b6ca5df36ccc5ff66fe81e0c017b866492
darkn1gth <lucasezequiel3097@gmail.com>
2018-03-26 22:13:12 -0300

    Corregido numeros primos

--- ProgramacionFrontEnd/Clase01/Ejercicios guia/ejer07.js
+++ ProgramacionFrontEnd/Clase01/Ejercicios guia/ejer07.js
@@ -1,15 +1,39 @@
 function NumerosPrimos() {
-    var contadorPrimos = 0;
-    for (var i = 2; contadorPrimos <= 20; i++) {
-        for (var k = 2; k < i / 2; k++) {
-            if (i % k == 0) {
-                break;
+    var numero = 1;
+    var contadorDivisores = 0;
+    var primeraVez;
+    var numerosPrimos = 0;
+    for (var numero_1 = 0; numerosPrimos < 20; numero_1++) {
+        primeraVez = false;
+        for (var i = 1; i < numero_1; i++) {
+            if (numero_1 % numero_1 == 0 && !primeraVez) {
+                contadorDivisores++;
+                primeraVez = true;
             }
+            if (numero_1 % i == 0)
+                contadorDivisores++;
         }
-        if (i % k != 0 || i == 2) {
-            contadorPrimos++;
-            console.log(i + "\n");
+        if (contadorDivisores == 2) {
+            numerosPrimos++;
+            console.log("Numero primo: " + numero_1 + " \n");
         }
+        contadorDivisores = 0;
     }
+    /* var contadorPrimos : number = 0;
+     for(var i : number = 2; contadorPrimos<=20;i++)
+     {
+         for(var k : number = 2; k<i/2;k++)
+         {
+             if(i%k==0)
+             {
+                 break;
+             }
+         }
+         if(i%k!=0 || i==2)
+         {
+             contadorPrimos++;
+             console.log(i + "\n");
+         }
+     }*/
 }
 NumerosPrimos();
--- ProgramacionFrontEnd/Clase01/Ejercicios guia/ejer07.ts
+++ ProgramacionFrontEnd/Clase01/Ejercicios guia/ejer07.ts
@@ -1,6 +1,32 @@
 function NumerosPrimos():void
 {
-    var contadorPrimos : number = 0;
+    let numero:number=1;
+    let contadorDivisores:number=0;
+    let primeraVez:Boolean;
+    let numerosPrimos:number=0;
+    for(let numero:number=0;numerosPrimos<20;numero++)
+    {
+        primeraVez=false;
+        for(let i:number=1;i<numero;i++)
+        {
+            if(numero%numero==0 && !primeraVez)
+            {
+                contadorDivisores++;
+                primeraVez=true;
+            }
+            if(numero%i==0)
+                contadorDivisores++;
+        }
+        if(contadorDivisores==2)
+        {
+            numerosPrimos++;
+            console.log(`Numero primo: ${numero} \n`);
+        }
+        contadorDivisores=0;
+    }
+    
+    
+   /* var contadorPrimos : number = 0;
     for(var i : number = 2; contadorPrimos<=20;i++)
     {
         for(var k : number = 2; k<i/2;k++)
@@ -15,7 +41,7 @@ function NumerosPrimos():void
             contadorPrimos++;
             console.log(i + "\n");
         }
-    }
+    }*/
 } 
 
 NumerosPrimos();
